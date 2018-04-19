01919646d2d7445a5b0c40c1bc813c8216806221
a5507203 <yaoyu5507203@gmail.com>
2018-04-19 23:38:13 +1000

    loading bar complete

--- js/UploadFile.js
+++ js/UploadFile.js
@@ -1,22 +1,25 @@
 var UploadFile = function( viewPort ) {
 
-    var sceneEl = this.sceneEl = viewPort.sceneEl;
-    this.PARTICLE_SIZE = 45;
-    var clusterNum = this.clusterNum = 0;
-    
-    var boundingSphereDict = {}
-    var pointsDict = {}
-    var normalizeParams = this.normalizeParams = {};  
+    var sceneEl  = viewPort.sceneEl;
+    var normalizeParams = {};  
     var container = viewPort.container;
     var pointContainer =  viewPort.pointContainer;
     var boundingSphereContainer = viewPort.boundingSphereContainer;
     var fileData = viewPort.fileData;
-
+    var confirm = null;
+    var noExtraFeatures = 0;
     //init GUI
     var gui = new dat.GUI({ width: 275, closeOnTop:true, name:'Upload File' } );
     var loader = document.createElement('div');
     loader.setAttribute('class','loading');
+    loader.style.display='none';
     sceneEl.appendChild(loader);
+
+
+    // var overlay = document.createElement('div');
+    // overlay.setAttribute('id','overlay');
+    // overlay.style.display='none';
+   // sceneEl.appendChild(overlay);
     //Load CSV file
     var uploadFileField = document.createElement("input");
     uploadFileField.setAttribute("id", "inputFileField");
@@ -33,11 +36,8 @@ var UploadFile = function( viewPort ) {
     slogan.innerHTML = 'Drive Into Your Data With <span style="color:#3e8e41">STARMAP</span>'
     sloganDiv.appendChild(slogan);
     
-    
     var demoDiv = this.demoDiv = document.createElement('div');
     demoDiv.setAttribute('class','demoDiv');
-    
-    
     
     var button = document.createElement('button');
     button.innerHTML = "Example One";
@@ -56,9 +56,8 @@ var UploadFile = function( viewPort ) {
     })
     demoDiv.appendChild(button2);
     
-    
+
     var button3 = document.createElement('button');
-    
     button3.innerHTML = "Example Three";
     button3.setAttribute('class','demoButton')
     button3.addEventListener('click',function( ) {
@@ -69,20 +68,16 @@ var UploadFile = function( viewPort ) {
     sceneEl.appendChild(sloganDiv);
     
     function demo(fileName) {
-        
+        loader.style.display='block';
         d3.text("sampleData/"+fileName, function(data) {
-    
      
             lines = data.split(/\r\n|\n/g);
-
             detectFeatures(lines[0]);
-
             convertToMatrix(lines);
-
             gui.destroy();
             sceneEl.removeChild(demoDiv);
             sceneEl.removeChild(sloganDiv);
-            
+            loader.style.display='none';
             viewPort.initControlUIAndRendering( );
             
         })
@@ -90,19 +85,18 @@ var UploadFile = function( viewPort ) {
     }
 
     function convertToMatrix ( rows ) {
+
         var keys = Object.keys(normalizeParams);
         var clusterIndex  = keys.indexOf('CLUSTER');
         keys.splice(clusterIndex, 1);
         var clusterColumnNum = normalizeParams['CLUSTER'].index;
         var clusterRecord = [];
-
-      
         for (var i = 1 ; i < rows.length -1 ; i += 1) {
 
             var columns = rows[i].split(",");
-
             var newCluster = columns[clusterColumnNum];
             clusterRecord.push(newCluster);
+            //detect different clusters and initialize render data
             if (!fileData.hasOwnProperty(newCluster)) {
                 fileData[newCluster] = new Points();
                 for (var key of keys) {
@@ -121,16 +115,13 @@ var UploadFile = function( viewPort ) {
                 if( currVal > currFeature.max)  currFeature.max = currVal;
                 if( currVal < currFeature.min)  currFeature.min = currVal;
             }
-
         }
-
         for ( var i = 0; i < keys.length ; i += 1 ) {
             var currFeature = normalizeParams[keys[i]];
             njMatrix = nj.array(currFeature.data);
             if ( keys[i] == "X" ||  keys[i] == "Y" || keys[i] == "Z" )
                 njMatrix = njMatrix.divide(currFeature.max).multiply(150)
                 
-            
             else njMatrix = njMatrix.subtract(currFeature.min).divide(currFeature.max-currFeature.min).multiply(0.48)
             currFeature.data = njMatrix.tolist();
        
@@ -161,16 +152,17 @@ var UploadFile = function( viewPort ) {
                 }
             }
 
-        }
-
-
-        
+        }    
+       
     }
 
 
-
-
     function detectFeatures( headerRow ) {
+        
+        if (confirm != null) {
+            gui.remove(confirm);
+            confirm = null;
+        }
         var headerList = headerRow.toUpperCase().split(",");
         if(headerList.indexOf('X') == -1 || headerList.indexOf('Y') == -1 || headerList.indexOf('Z') == -1 || headerList.indexOf('CLUSTER') == -1) 
         {
@@ -178,9 +170,14 @@ var UploadFile = function( viewPort ) {
             // console.log(headerList.indexOf('Y') )
             // console.log(headerList.indexOf('Z') )
             // console.log(headerList[9]== 'CLUSTER')
-            console.log(headerList)
+
             return false;
         }
+        // var realHeaderList = [];
+
+        // for ( var i = 0 ; i < headerList.length ; i += 1 ) {
+
+        // }
         for( var i = 0 ; i < headerList.length ; i += 1 ) {
             var currFeature = headerList[i];
             if( currFeature.length == 0 || currFeature == '""' ) continue;
@@ -199,54 +196,56 @@ var UploadFile = function( viewPort ) {
 
 
         }
+        if(Object.keys(normalizeParams).length == 4) {
+
+            addAxisAsFeature();
+        }
         console.log(normalizeParams)
         return true;
 
     }
+
+    function addAxisAsFeature() {
+        var axisList = ['X_AXIS','Y_AXIS','Z_AXIS'];
+        var indexs = [normalizeParams.X.index,normalizeParams.Y.index,normalizeParams.Z.index];
+        for ( var i = 0 ; i < 3 ; i += 1 ){
+
+
+            currFeature = axisList[i]
+
+            var feature = {};
+            feature[currFeature] = function(){};
+            config.featureMap.push(feature);
+            normalizeParams[currFeature] = {};
+            normalizeParams[currFeature].data = [];
+            normalizeParams[currFeature].index = indexs[i];
+            normalizeParams[currFeature].max = -Infinity;
+            normalizeParams[currFeature].min = Infinity;
+        }
+    }
     
     uploadFileField.onchange = function(evt) {
         
-  
+        loader.style.display='block';
+        fileData = viewPort.fileData = {};
+        noExtraFeatures = 0;
         var inputFile = evt.target.files[0];
         var reader = new FileReader();
 
+        normalizeParams = {};  
+
         reader.onload = function(e) {
-
             data = reader.result;
-            
             lines = data.split(/\r\n|\n/g);
-
             if (detectFeatures(lines[0])){
-
                 if(convertToMatrix(lines)){
                    confirmUI();
-      
-
-
                 }
             }
-            
-
-            uploadFileField.value='';
-            
-
-            // gui.destroy();
-            // sceneEl.removeChild(demoDiv);
-            // sceneEl.removeChild(sloganDiv);
-
-            // viewPort.initControlUIAndRendering( );
-            // console.log(reader.result);
-            // var data = d3.csvParse(reader.result);
-
-            // if(preProcessData(data)) confirmUI();
-            // else uploadFileField.value='';
-            // console.log('end');
-            
-            
-        }
-                    
+            loader.style.display='none';
+            uploadFileField.value='';     
+        }           
         reader.readAsText(inputFile);
-
     };
 
     var uploadFileParams = {
@@ -258,148 +257,24 @@ var UploadFile = function( viewPort ) {
     gui.add(uploadFileParams,'loadFile').name('Load CSV file');
 
 
-
-    //data process, store points to an object
-    function preProcessData(data) {
-        //error detection
-        if(!data[0].hasOwnProperty('x') || !data[0].hasOwnProperty('y') || !data[0].hasOwnProperty('z') || !data[0].hasOwnProperty('cluster')) 
-
-          // TODO write text on screen
-          return false;
+    var fileUploaded = function (){
+        //boundingBoxEl.setAttribute( 'visible', false );
+        gui.destroy();
+        sceneEl.removeChild(demoDiv);
+        sceneEl.removeChild(sloganDiv);
+        viewPort.initControlUIAndRendering( );
+        //location.reload();
         
-
-        for(var key in data[0]) {
-            
-            if( !data[0].hasOwnProperty(key) ) continue;
-            if( key =='cluster' || key == '' ) continue;
-            if( key != 'x' && key !='y' && key != 'z' ) {
-                var feature = {};
-                feature[key] = function(){};
-                config.featureMap.push(feature);
-                
-            }
-            normalizeParams[key] = {};
-            normalizeParams[key].max = -Infinity;
-            normalizeParams[key].min = Infinity;
-            //normalizeParams[key].mean = 0;
-            
-        }
-        
-        
-        var len = data.length;
-        
-        for(var i = 0; i < len; i+=1 ) {
-            var p = data[i];
-            var index = p.cluster;
-            
-            if (!updateNormalizeParams(p))  return false;
-            
-
-            
-            if (!fileData.hasOwnProperty(index)) {
-                fileData[index] = new Points();
-                for (var key of Object.keys(normalizeParams)) {
-                    if(key == 'x' || key == 'y' || key == 'z' ) continue;
-                    fileData[index].features[key] = [];
-                }
-            }
-            
-        }
-        
-    
-        for (var key of Object.keys(normalizeParams)) {
-            
-            normalizeParams[key].range = normalizeParams[key].max - normalizeParams[key].min;
-            //value.mean = value.mean/len;
-        }
-        
-        //console.log(normalizeParams);
-        var keys = Object.keys(normalizeParams);
-        var keyLen = keys.length;
-        viewPort.featuresNum = keyLen - 3;
-        
-        for(var i = 0; i < len; i++ ) {
-            var p = data[i];
-            var index = p.cluster;
-            var currCluster = fileData[index];
-            for ( var j = 0; j < keyLen; j+=1 ) {   
-                var currParamName = keys[j];
-          
-                if ( currParamName == 'x' || currParamName == 'y' || currParamName == 'z') { 
-                    p[currParamName] = normlizeParam(p[currParamName],normalizeParams[currParamName].max,150);
-                }
-                else if(p.cluster != -1) {
-                    p[currParamName] = normlizeFeature(p[currParamName],normalizeParams[currParamName],0.48);
-                    currCluster.features[currParamName].push(p[currParamName]);
-                }
-                
-            }
-            currCluster.positions.push(p.x,p.y,p.z);
-        }
-
-        console.log(fileData)
-        return true;
     }
-    
+
+    var confirmLogic = { startToExplore : fileUploaded };
  
-    
-    function updateNormalizeParams(p){
-        
-        for (var key of Object.keys(normalizeParams)) {
-            
-            var val = normalizeParams[key];
-            var currVal = parseFloat(p[key]);
-            if (isNaN(currVal)) return false;
-            if( currVal > val.max) val.max = currVal;
-            if( currVal < val.min) val.min = currVal;
-            
-            //if(key =='x' || key == 'y' || key == 'z' )  val.mean += currVal;
-                
-            
-            
-        }
-        return true
-        
-    }
-    
-    function normlizeParam(value, range, newRange) {
-        if( range == 0 ) return parseFloat(value); 
-        return simplify(value/range*newRange)
-        
-    }
-    
-    function normlizeFeature(value, range, newRange) {
-        
-        return simplify(((value-range.min)/range.range)*newRange)
-        
-    }
-    
     function confirmUI() {
 
         // confirm button to enter the VR 
-        var fileUploaded = function (){
-            //boundingBoxEl.setAttribute( 'visible', false );
-            gui.destroy();
-            sceneEl.removeChild(demoDiv);
-            sceneEl.removeChild(sloganDiv);
-            viewPort.initControlUIAndRendering( );
-            //location.reload();
-           
-        }
-
-        var obj = { startToExplore : fileUploaded };
-        gui.add(obj,'startToExplore').name("confirm");
-        
-    }
-
-    function simplify(num) {
-
-        return Math.round10(parseFloat(num),-2);
-        
-    }
-
-    function normalizeData( data) {
-        
+        confirm = gui.add(confirmLogic,'startToExplore').name("confirm");
+     
+   
         
     }
 
